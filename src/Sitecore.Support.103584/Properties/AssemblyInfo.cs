847632aa2036eb6f1e1719e7fb6fa051c285e705
DK\yho <yho@sitecore.net>
2017-08-14 09:53:04 +0800

    If you press the Enter key in a Multi-Line Text field when editing it in Experience Editor, two lines (<br> tags) are added instead of one for IE version.

---
+++ src/Sitecore.Support.103584/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.103584")]
+[assembly: AssemblyProduct("Sitecore.Support.103584")]
+[assembly: ComVisible(false)]
